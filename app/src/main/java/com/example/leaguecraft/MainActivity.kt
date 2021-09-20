19174612e61e7cdf8abe51b269079140307b0a2f
Ecnavda <ecnavda@gmail.com>
2021-09-19 22:26:29 -0400

    Added ItemBuild activity and placeholder image for the item selections.

--- app/src/main/java/com/example/leaguecraft/MainActivity.kt
+++ app/src/main/java/com/example/leaguecraft/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.leaguecraft
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val get_started = findViewById<Button>(R.id.homeButton)
+
+        get_started.setOnClickListener {
+            val i = Intent(this, ItemBuild::class.java)
+            this.startActivity(i)
+        }
     }
 }
